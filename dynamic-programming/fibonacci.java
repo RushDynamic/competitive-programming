eb39629a06dc124bcc1b6216b383e55f7d951aa0
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-08-29 03:34:34 +0530

    Memoized fibonacci

--- dynamic-programming/fibonacci.java
+++ dynamic-programming/fibonacci.java
@@ -1,11 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class fibonacci {
     public static void main(String[] args) {
-        System.out.println("Result: " + calculateFib(50));
+        Map<Integer, Double> resultMap = new HashMap<Integer, Double>();
+        System.out.println("Result: " + calculateFib(50, resultMap));
     }
 
-    private static int calculateFib(int n) {
+    private static Double calculateFib(int n, Map<Integer, Double> resultMap) {
         if (n <= 2)
-            return 1;
-        return (calculateFib(n - 1) + calculateFib(n - 2));
+            return 1.0;
+        if (resultMap.containsKey(n))
+            return resultMap.get(n);
+        else
+            resultMap.put(n, calculateFib(n - 1, resultMap) + calculateFib(n - 2, resultMap));
+        // return (calculateFib(n - 1, resultMap) + calculateFib(n - 2, resultMap));
+        return resultMap.get(n);
     }
 }

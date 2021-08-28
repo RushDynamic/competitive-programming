4853427e5997ebd9c42b211028e9b22455e39bef
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-08-29 05:19:18 +0530

    Memoized Grid Traversal

--- dynamic-programming/GridTraversal.java
+++ dynamic-programming/GridTraversal.java
@@ -1,13 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class GridTraversal {
     public static void main(String[] args) {
-        System.out.println("Result: " + traverse(18, 18));
+        Map<String, Double> resultSet = new HashMap<String, Double>();
+        System.out.println("Result: " + traverse(18, 18, resultSet));
     }
 
-    private static int traverse(int rows, int cols) {
+    private static Double traverse(int rows, int cols, Map<String, Double> resultSet) {
         if (rows == 0 || cols == 0)
-            return 0;
+            return Double.valueOf(0);
         if (rows == 1 && cols == 1)
-            return 1;
-        return (traverse(rows - 1, cols) + traverse(rows, cols - 1));
+            return Double.valueOf(1);
+        if (resultSet.containsKey(rows + "," + cols)) {
+            // System.out.println("Already memoized");
+            return resultSet.get(rows + "," + cols);
+        }
+        // System.out.println("Not memoized");
+        resultSet.put(rows + "," + cols, traverse(rows - 1, cols, resultSet) + traverse(rows, cols - 1, resultSet));
+        return resultSet.get(rows + "," + cols);
     }
 }
